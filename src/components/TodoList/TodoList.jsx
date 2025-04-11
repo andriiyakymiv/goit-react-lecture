4f9e8e88b94223021dad2d0bad9fdd9de9583f41
andrii <a.iakumiv@gmail.com>
2025-04-11 21:43:28 +0300

    Add function add todo list for components ToDo

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -6,6 +6,7 @@ import s from './TodoList.module.css';
 export const TodoList = () => {
 
   const [todos, setTodos] = useState(todosData);
+  const [todoVelue, setTodoValue] = useState('');
 
   const handleDeleteItem = id => {
     console.log(`Delete item logic ${id}`);
@@ -13,14 +14,24 @@ export const TodoList = () => {
     setTodos(newData);
   };
 
+  const addTodo = () => {
+    const newToDo = {
+      id: crypto.randomUUID(),  
+      todo: todoVelue,
+      completed: false
+    }
+    setTodos(...todos, newToDo);
+    setTodoValue('');
+  }
+
   return (
     <div>
       <div className='flex'>
-        <input className={s.input} />
-        <button className='btn border'>Add</button>
+        <input value={todoVelue} onChange={event => setTodoValue(event.target.value)} className={s.input} />
+        <button onClick={addTodo} className='btn border'>Add</button>
       </div>
       <ul className={s.list}>
-        {todos.map(item => (
+        {todos.map(item => ( 
           <TodoItem key={item.id} {...item} handleDeleteItem={handleDeleteItem} />
         ))}
       </ul>
