520f0e79b2dc29be2d323cb747373103b5bf3123
andrii <a.iakumiv@gmail.com>
2025-04-12 08:17:22 +0300

    Fix button add logic for component ToDoList

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,28 +2,28 @@ import { useState } from 'react';
 import todosData from '../../assets/todos.json';
 import { TodoItem } from './TodoItem';
 import s from './TodoList.module.css';
-
+// ----------------------------------------------
 export const TodoList = () => {
-
+// ----------------------------------------------
   const [todos, setTodos] = useState(todosData);
   const [todoVelue, setTodoValue] = useState('');
-
+// ----------------------------------------------
   const handleDeleteItem = id => {
     console.log(`Delete item logic ${id}`);
     const newData = todos.filter(item => item.id !== id);
     setTodos(newData);
   };
-
+// ----------------------------------------------
   const addTodo = () => {
     const newToDo = {
       id: crypto.randomUUID(),  
       todo: todoVelue,
       completed: false
     }
-    setTodos(...todos, newToDo);
+    setTodos([...todos, newToDo]);
     setTodoValue('');
   }
-
+// ----------------------------------------------
   return (
     <div>
       <div className='flex'>
